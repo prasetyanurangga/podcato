4a26b67d5fdf6f29f01b15a4a704f0a66d0bfeba
Angga Nur Prasetya <angganurprasetya4@gmail.com>
2023-02-13 03:03:30 +0700

    Try Listview

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:transformable_list_view/transformable_list_view.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -8,14 +9,65 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  Matrix4 getScaleDownMatrix(TransformableListItem item) {
+    /// final scale of child when the animation is completed
+    const endScaleBound = 0.75;
+
+    /// 0 when animation completed and [scale] == [endScaleBound]
+    /// 1 when animation starts and [scale] == 1
+    final animationProgress = item.visibleExtent / item.size.height;
+
+    /// result matrix
+    final paintTransform = Matrix4.identity();
+
+    /// animate only if item is on edge
+    if (item.position != TransformableListItemPosition.middle &&
+        item.position != TransformableListItemPosition.bottomEdge) {
+      final scale = endScaleBound + ((1 - endScaleBound) * animationProgress);
+
+      paintTransform
+        ..translate(item.size.width / 2)
+        ..scale(scale)
+        ..translate(-item.size.width / 2);
+    }
+
+    return paintTransform;
+  }
+
   Widget _buildBody() {
     return Container(
-        child: Column(
-      children: [
-        const Text("Podcato"),
-        Container(child: const Text("Popular"))
-      ],
-    ));
+      child: Column(
+        children: [
+          const Text("Podcato"),
+          Container(
+            child: const Text(""),
+          ),
+          Expanded(
+            child: TransformableListView.builder(
+              controller: ScrollController(),
+              padding: EdgeInsets.zero,
+              getTransformMatrix: getScaleDownMatrix,
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 100,
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 4,
+                  ),
+                  decoration: BoxDecoration(
+                    color: index.isEven ? Colors.grey : Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  alignment: Alignment.center,
+                  child: const Text("tttt"),
+                );
+              },
+              itemCount: 30,
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   @override
